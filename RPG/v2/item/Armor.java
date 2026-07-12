1f7bd0b937d3c96e02bd94f2217162deb31b641e
Musc <97372099+Melanierr@users.noreply.github.com>
2026-07-12 16:04:09 +0700

    fixed defense calculations and format

--- RPG/v2/item/Armor.java
+++ RPG/v2/item/Armor.java
@@ -4,14 +4,17 @@ public class Armor extends Item {
     double defense;
     public Armor(String name, String description, int amount, double defense){
         super(name, description, amount);
-        this.defense = defense / 100;
+        this.defense = defense;
     }
-    public double getDefense(){
-        return defense;
+    public double getDefMultiplier(){
+        return 1 - (defense / 100);
     }
     @Override
     public void useItem(entity.Character player) {
-        System.out.println("Armor has been equipped! Taken less " + getDefense() + "x DMG");
-        this.amount -= 1;
+        System.out.println(this.name + " equipped! DEF increased " + defense + "%");
+        player.setArmorEquipped(this);
+    }
+    public String toSaveString() {
+        return "Armor," + getName() + "," + getAmount() + "," + getDefMultiplier();
     }
 }
