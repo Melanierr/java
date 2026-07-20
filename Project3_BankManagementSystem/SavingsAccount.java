251b09d82e1e0c84e74dc84b774c596733be954e
Musc <97372099+Melanierr@users.noreply.github.com>
2026-07-20 13:44:23 +0700

    savings account will now have a 3% interest rate per month, will be editable in the future LOL!!!

--- Project3_BankManagementSystem/SavingsAccount.java
+++ Project3_BankManagementSystem/SavingsAccount.java
@@ -1,5 +1,16 @@
 public class SavingsAccount extends BankAccount{
+    private double monthlyInterestRate = 3; // 3% per month
+    private int monthPeriod = 1;
+    private String accountType = "savingsaccount";
     SavingsAccount(String name, String accountNumber, double balance){
         super(name,accountNumber,balance);
+    }
+    // profit will be monthly, not yearly yet
+    double calculateMoneyInterest(){
+        double totalMoney = (this.getBalance() * (monthlyInterestRate / 100) * monthPeriod);
+        return totalMoney;
+    }
+    public String getAccountType(){
+        return accountType;
     }
 }
