a81ea92ade9efd34c77c785ef89547b75937a42e
Musc <97372099+Melanierr@users.noreply.github.com>
2026-08-04 16:51:00 +0700

    addded foolproof to adding items amoutn that is lower than 0, sorting amount is now from the most to the least

--- Project5_InventoryConsole/InventoryManager.java
+++ Project5_InventoryConsole/InventoryManager.java
@@ -1,31 +1,28 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class InventoryManager {
-    private Scanner sc;
     private ArrayList<Item> inventory;
-
-    public InventoryManager(Scanner scanner){
-        this.sc = scanner;
+    public InventoryManager(){
         this.inventory = new ArrayList<>();
     }
-    public void addItem(String name, String type, int amount, double price){
+    public void addItem(String name, String type, int amount){
+        if(amount < 0){
+            System.out.println("Count must be larger than 0.");
+            return;
+        }
         for(Item item : inventory){
             if(item.getName().equalsIgnoreCase(name)){
                 if(item.getAmount() + amount > Item.maxStack){
                     System.out.println("Stack limit exceeded.");
                     return;
                 }
-
                 item.addAmount(amount);
                 return;
             }
         }
-
-        inventory.add(new Item(name, type, amount, price));
+        inventory.add(new Item(name, type, amount));
     }
-
     public void deleteItem(String name, int amount){
         if(inventory.isEmpty()){
             System.out.println("Inventory is empty.");
@@ -38,11 +35,9 @@ public class InventoryManager {
                     System.out.println("Not enough items to delete.");
                     return;
                 }
-
                 if(item.getAmount() == 0){
                     target = item;
                 }
-
                 System.out.printf("Removed %s x%d%n", name, amount);
                 break;
             }
@@ -50,19 +45,6 @@ public class InventoryManager {
         if(target != null){
             inventory.remove(target);
         }
-    }
-
-    public Item searchItemID(String id){
-        if(inventory.isEmpty()){
-            System.out.println("Inventory is empty.");
-            return null;
-        }
-        for(Item item : inventory){
-            if(item.getId().equalsIgnoreCase(id)){
-                return item;
-            }
-        }
-        return null;
     }
     public Item searchItemName(String name){
         if(inventory.isEmpty()){
@@ -77,25 +59,17 @@ public class InventoryManager {
         return null;
     }
     public void sortItem(String sortType){
-        switch(sortType){
-            case "name" -> getInventory().sort(Comparator.comparing(Item::getName));
-            case "amount" -> getInventory().sort(Comparator.comparingInt(Item::getAmount));
-            case "price" -> getInventory().sort(Comparator.comparingDouble(Item::getPrice));
+        switch(sortType.toLowerCase()){
+            case "name" -> inventory.sort(Comparator.comparing(Item::getName));
+            case "amount" -> inventory.sort(Comparator.comparingInt(Item::getAmount).reversed());
+            case "price" -> inventory.sort(Comparator.comparingDouble(Item::getPrice));
+            default -> {
+                System.out.println("Invalid sort type.");
+                return;
+            }
         }
         viewInventory();
     }
-    /* public void viewByType(String type){ old version
-
-        ArrayList<Item> tempList = new ArrayList<>();
-        for(Item item : inventory){
-            if(item.getType().equalsIgnoreCase(type)){
-                tempList.add(item);
-            }
-        }
-        for(Item item : tempList){
-            System.out.println(item);
-        }
-    } */
     public void viewByType(String type){ // new version
         boolean found = false;
         for(Item item : inventory){
@@ -108,7 +82,6 @@ public class InventoryManager {
             System.out.println("No items found in this category.");
         }
     }
-
     public boolean viewInventory(){
         if(inventory.isEmpty()){
             System.out.println("Inventory is empty.");
